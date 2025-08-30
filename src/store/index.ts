c46f1ea5293e5d926bb73056f696e38d24cf42da
innces <info@nasih-network.com>
2025-08-30 10:30:42 +0300

    more enhancements

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import UserInfo from './slices/UserInfo.slice';
-import ServicesAPIs from './slices/ServicesAPIS.slice'
 
 const store = configureStore({
     reducer : {
         // current user data 
         UserInfoData: UserInfo.reducer,
-        // services api urls 
-        ServicesAPIs: ServicesAPIs.reducer
     }
 });
 
